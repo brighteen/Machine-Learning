801a0ee4bf2ff6ef28bdf3e382e21d953d14f94b
여명구 <88411212+brighteen@users.noreply.github.com>
2025-03-31 21:37:13 +0900

    발표준비

--- genetic_algorithm/ch5/5. inversion.py
+++ genetic_algorithm/ch5/5. inversion.py
@@ -1,22 +1,24 @@
 import copy
 import random
 
-def mutation_inversion(ind):
-    # 원본 개체(ind)를 deepcopy하여 새로운 개체(mut)를 생성
+def mutation_inversion(ind): # 개체(ind, 리스트)를 인자로 받음음 
+    # 원본 데이터를 유지하기 위해 복사본 생성
     mut = copy.deepcopy(ind)
     # 변이 전 순서를 보존하기 위해 원본 복사본 생성
     temp = copy.deepcopy(ind)
+    print(f'[debug] 초기 개체 : {ind}')
     # 리스트 내에서 두 인덱스를 선택하고 오름차순 정렬 (i, j)
     pos = sorted(random.sample(range(0, len(mut)), 2))
+    print(f'[debug] 두 인덱스를 선택(개체x) : {pos}')
     # pos[0]부터 pos[1]까지의 구간을 역순으로 재배치
     for i in range(0, (pos[1] - pos[0]) + 1):
         # 수식: new_gene[pos[0]+i] = original_gene[pos[1]-i]
         mut[pos[0] + i] = temp[pos[1] - i]
+        print(f'[debug] 기존 개체[{pos[0] + i}] = 변이 후 개체[{pos[1] - i}]')
     return mut
 
-# 예시 실행 코드
 if __name__ == '__main__':
-    random.seed(5)
+    random.seed(3)
     # 1부터 5까지의 숫자로 구성된 순열 개체 생성
     ind = list(range(1, 6))
     mut = mutation_inversion(ind)
--- genetic_algorithm/ch5/6. shuffle.py
+++ genetic_algorithm/ch5/6. shuffle.py
@@ -2,21 +2,24 @@ import copy
 import random
 
 def mutation_shuffle(ind):
-    # 원본 개체(ind)를 deepcopy하여 새로운 개체(mut)를 생성
+    # 원본 개체(ind)를 유지하면서 변이 진행
     mut = copy.deepcopy(ind)
+    print(f'[debug] 초기 개체 : {ind}')
     # 리스트 내에서 두 인덱스를 선택하고 오름차순 정렬 (i, j)
     pos = sorted(random.sample(range(0, len(mut)), 2))
+    print(f'[debug] 두 인덱스를 선택(개체x) : {pos}')
     # 선택된 구간 [i, j]의 서브리스트 추출
     subrange = mut[pos[0]:pos[1] + 1]
+    print(f'[debug] 변이 전 서브리스트 : {subrange}')
     # 추출된 서브리스트를 무작위로 섞음 (내부 순서 변화)
     random.shuffle(subrange)
     # 섞인 서브리스트를 원래 위치에 대입
     mut[pos[0]:pos[1] + 1] = subrange
+    print(f'[debug] 변이 후 서브리스트 : {mut[pos[0]:pos[1] + 1]}')
     return mut
 
-# 예시 실행 코드
 if __name__ == '__main__':
-    random.seed(13)
+    random.seed(3)
     # 1부터 5까지의 숫자로 구성된 순열 개체 생성
     ind = list(range(1, 6))
     mut = mutation_shuffle(ind)
